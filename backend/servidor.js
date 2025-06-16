889626ba4a914dcb595e1b077c9d31fa983f4473
William dos Santos Rodrigues <williamdossantosrodrigues94@gmail.com>
2025-06-16 11:32:32 -0300

    comentarios no servidor.js

--- backend/servidor.js
+++ backend/servidor.js
@@ -21,6 +21,7 @@ const rotasSessoes = require("./routes/rotas-sessao");
 const rotasCadeiras = require("./routes/rotas-cadeiras");
 const rotasReservas = require("./routes/rotas-reservas");
 const rotasIngressos = require("./routes/rotas-ingressos");
+//const rotasPagamentos = require("./routes/rotas-pagamentos");
 
 // Definindo as rotas do servidor
 app.use('/api/auth', rotasAutenticacao);
@@ -30,6 +31,7 @@ app.use('/api/sessoes', rotasSessoes);
 app.use('/api/cadeiras', rotasCadeiras);
 app.use("/api/reservas", rotasReservas);
 app.use("/api/ingressos", rotasIngressos);
+//app.use("/api/pagamentos", rotasPagamentos);
 
 // Definindo a rota raiz
 const PORTA = process.env.PORT || 3001;
